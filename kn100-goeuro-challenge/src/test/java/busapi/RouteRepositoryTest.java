9dec6963c583c45b0a6e74ea59be961432a65acc
Kevin <787037@swan.ac.uk>
2016-12-03 15:50:48 +0000

    Fixed formatting

--- kn100-goeuro-challenge/src/test/java/busapi/RouteRepositoryTest.java
+++ kn100-goeuro-challenge/src/test/java/busapi/RouteRepositoryTest.java
@@ -33,6 +33,7 @@ public class RouteRepositoryTest {
         StringReader input = new StringReader("2\n1 1 1 2 3 4\n2 4 3 2 1\n3 9 7 8 6");
         BufferedReader inputbr = new BufferedReader(input);
         RouteRepository testRR = new RouteRepository(inputbr);
+        
         String resp = testRR.routePossible("4","2");
         String correctresp = "{\"dep_sid\":4,\"arr_sid\":2,\"direct_bus_route\":true}";
         assertEquals(correctresp,resp);
